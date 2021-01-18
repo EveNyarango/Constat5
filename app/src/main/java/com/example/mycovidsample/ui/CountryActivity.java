8d02d18d1badb991bb5e628fdbc0c86997c88574
EveNyarango <evenyarango@gmail.com>
2021-01-18 23:09:59 +0300

    initial commit

---
+++ app/src/main/java/com/example/mycovidsample/ui/CountryActivity.java
@@ -0,0 +1,16 @@
+package com.example.mycovidsample.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mycovidsample.R;
+
+public class CountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+    }
+}
