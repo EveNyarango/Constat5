668d393ab121a4eba9ec6b4e18ef2857690ff99c
EveNyarango <evenyarango@gmail.com>
2021-01-31 17:13:13 +0300

    add animation

--- app/src/main/java/com/example/mycovidsample/ui/SplashActivity.java
+++ app/src/main/java/com/example/mycovidsample/ui/SplashActivity.java
@@ -2,15 +2,51 @@ package com.example.mycovidsample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mycovidsample.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
+
+    //    variables
+    Animation topAnim, bottomAnim;
+    ImageView cona;
+    TextView heading, designer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        Animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+//        hooks
+        cona = findViewById(R.id.cona);
+        heading = findViewById(R.id.heading);
+        designer = findViewById(R.id.designer);
+
+        cona.setAnimation(topAnim);
+        heading.setAnimation(bottomAnim);
+        designer.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
