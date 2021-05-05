6c3486bf0ce9b0322b53e2a62706f43d1b2d0a7e
EveNyarango <evenyarango@gmail.com>
2021-05-05 13:38:27 +0300

    done ready to host

--- app/src/main/java/com/example/mycovidsample/ui/LoginActivity.java
+++ app/src/main/java/com/example/mycovidsample/ui/LoginActivity.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -56,6 +59,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+
 //        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 //        mEditor = mSharedPreferences.edit();
 
@@ -85,6 +89,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
         createAuthProgressDialog();
+
     }
 
     public void saveLocationToFirebase(String location) {
